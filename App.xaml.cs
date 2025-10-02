24ebb7da758cf42c0139c3f51031eeae8b089bc6
Hugop2san <hugopereira2santos60@gmail.com>
2025-10-02 15:43:50 -0300

    projeto main maui

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace kogui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
